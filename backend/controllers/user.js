61fac59e74bc14bc505717e08083bf0fbc0d3891
OTTIN <ottin.timothe@gmail.com>
2021-01-10 23:26:05 +0100

    V1.5
    Création d'un compte quand la BDD est vide, aucun problème.
    Impossible de creer + de 1 compte, mais si ce problème résolu tout le reste aussi.
    Problème au niveau des promise une n'a pas de retour.
    
    La V1.4 était stable mais un même email pouvais s'inscrire plusieurs fois et créer un user en plus dans la BDD

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,105 +5,113 @@ const User = require('../models/User') //On importe nos models User
 
 //On exporte notre fonction qui gère une route et on lui donne un nom
 exports.signup = (req, res, next) => { //Middleware signup
-
-          User.find()
-          .then(users => {
-               users.forEach(user => {
-                    bcrypt.compare(req.body.email, user.email)
-                    .then(valid => {
-                         if(!valid){
-                              bcrypt.hash(req.body.password, 10) //On appel la fonction de hash de bcrypt qui va "saler" le mot de passe 10 fois
-                              .then(hash => {
-                                   const user = new User({ //On créé un nouvel utilisateur avec le model mongoose
-                                        email: req.body.email, //email fourni dans le corps de la requête
-                                        password: hash //On enregistre le hash récupéré
-                                   })
-                                   console.log("user après hash password", user)
-                                   bcrypt.hash(user.email, 10)
-                                   .then(hash => {
-                                        user.update(user.email = hash)
-                                        console.log("user après hash email et avant save", user)
+     User.find()
+     .then(users => {
+          // console.log("users dans le find (vérifie bien du coup)", users)
+          console.log("Taille du tableau users au find", users.length)
+          if(users.length === 0){ // Si taille de la bdd === 0 on créé un user
+               console.log("Si la taille de users = 0 on créé un new user", users)
+               bcrypt.hash(req.body.password, 10) //On appel la fonction de hash de bcrypt qui va "saler" le mot de passe 10 fois
+               .then(hash => {
+                    const user = new User({ //On créé un nouvel utilisateur avec le model mongoose
+                         email: req.body.email, //email fourni dans le corps de la requête
+                         password: hash //On enregistre le hash récupéré
+                    })
+                    // console.log("user après hash password", user)
+                    bcrypt.hash(user.email, 10)
+                    .then(hash => {
+                         user.update(user.email = hash)
+                         // console.log("user après hash email et avant save", user)
+                         user.save() //On enregistre le nouvel utilisateur dans la bdd
+                         .then(user => {
+                              // console.log("user save", user)
+                              res.status(201).json({ message: 'Utilisateur créé !' })
+                         })
+                         .catch(error => res.status(401).json({ error })) //Erreur Bad Request
+                    })
+                    .catch(error => res.status(401).json({ error })) //Erreur Bad Request
+               })
+               .catch(error => res.status(500).json({ error }))
+          } else if (users.length >= 1){ //Si bdd a au moins un user
+               // console.log("Chaque user si la taille du tableau users est >= 1", users)
+               // const userExist = [] //Initie un tableau vide
+               // const userNew = []
+               // console.log("userNew au debut", userNew)
+               // console.log("userExist au départ", userExist)
+               bcrypt.hash(req.body.password, 10) //On appel la fonction de hash de bcrypt qui va "saler" le mot de passe 10 fois
+               .then(hash => {
+                    // console.log("Tableau users dans le hash", users)
+                    const user = new User({ //On créé un nouvel utilisateur avec le model mongoose
+                         email: req.body.email, //email fourni dans le corps de la requête
+                         password: hash //On enregistre le hash récupéré
+                    })
+                    // console.log("user après hash password", user)
+                    // console.log("Tableau users apres le hash", users)
+                    bcrypt.hash(user.email, 10)
+                    .then(hash => {
+                         // console.log("Tableau users au départ", users)
+                         // console.log("user au départ", user)
+                         user.update(user.email = hash)
+                         // console.log("user après hash email et avant save", user)
+                         // console.log("user après hash email et avant save", user)
+                         // console.log("Tableau users apres le hash email et avant save", users)
+                         users.forEach(userTest => { //Pour chaque user de users
+                              // console.log("userTest dans la boucle", userTest)
+                              // console.log("Tableau users dans la boucle", users)
+                              // console.log("user dans la boucle", user)
+                              // req.body.email = user.email
+                              console.log("user.email dans la boucle")
+                              console.log("user.email dans la boucle", user.email)
+                              
+                              console.log("userTest.email dans la boucle")
+                              console.log("userTest.email dans la boucle", userTest.email)
+                              
+                              bcrypt.compare(user.email, userTest.email) //On compare les emails
+                              .then(valid => {   
+                                   console.log("valid apres le then !!!!!!!!!!!!!!!!!", valid)
+                                   // console.log("Le userTest qui prend le compare", userTest.email)
+                                   if(!valid){ //Si comparaison valid l'user existe déja
+                                        // console.log("valid si compare valid", valid)
+                                        // console.log("Le user qui prend le compare", userTest.email)
+                                        // userExist.push(user)
+                                        console.log("userTest.email Avant save")
+                                        console.log("userTest.email Avant save", userTest.email)
+     
+                                        console.log("user Avant save")
+                                        console.log("user Avant save", user.email)
+     
                                         user.save() //On enregistre le nouvel utilisateur dans la bdd
                                         .then(user => {
                                              console.log("user save", user)
+                                             // console.log("users save", users)
+                                             // if(user){
                                              res.status(201).json({ message: 'Utilisateur créé !' })
+                                             console.log("user save §§§§§§§§§§§§", user)
+
+                                             // }
                                         })
                                         .catch(error => res.status(401).json({ error })) //Erreur Bad Request
-                                   })
-                                   .catch(error => res.status(401).json({ error })) //Erreur Bad Request
+                                   } 
+                                   // console.log("Le user qui prend le compare si false", userTest.email)
+                                   // userNew.push(user)
+                                   res.status(401).json({ message: 'Utilisateur existant !' })
+
                               })
-                              .catch(error => res.status(500).json({ error }))
-                         } else if (valid){
-                              return res.status(401).json({ error: 'Utilisateur existant !' }) //Erreur Unauthorized
-                         }
+                              .catch(error => res.status(500).json({ error })) //Erreur Internal Server Error 
+                         })
                     })
-                    .catch(error => res.status(500).json({ error })) //Erreur Internal Server Error 
+                    .catch(error => res.status(401).json({ error })) //Erreur Bad Request
                })
-          })
-          .catch(error => req.status(500).json({ error }))
-
-
-
-          // // On hash le mdp
-          // bcrypt.hash(req.body.password, 10) //On appel la fonction de hash de bcrypt qui va "saler" le mot de passe 10 fois
-          // .then(hash => {  //On récupère le hash créé par bcrypt
-          //      const user = new User({ //On créé un nouvel utilisateur avec le model mongoose
-          //           email: req.body.email, //email fourni dans le corps de la requête
-          //           password: hash //On enregistre le hash récupéré
-          //      })
-          //      console.log("user après hash password", user)
-          //      bcrypt.hash(user.email, 10)
-          //      .then(hash => {
-          //           user.update(user.email = hash)
-          //           console.log("user après hash email ", user)
-          //           console.log("user avant save", user)
-          //           user.save() //On enregistre le nouvel utilisateur dans la bdd
-          //           .then(user => {
-          //                console.log("user save", user)
-          //                res.status(201).json({ message: 'Utilisateur créé !' }) //On renvoie une création de ressource
-          //           }) //On renvoie une création de ressource
-          //           .catch(error => res.status(401).json({ error })) //Erreur Bad Request
-          //      })
-          //      .catch(error => res.status(401).json({ error })) //Erreur Bad Request
-          // })
-          // .catch(error => res.status(500).json({ error }))
-
-
-          // User.find()
-          //                .then(users => {
-          //                     console.log("users tableau", users)
-          //                     bcrypt.compare(req.body.email, user.email)  
-          //                     .then(valid => {
-          //                          if(valid){
-          //                               return res.status(401).json({ error: 'Utilisateur existant !' }) //Erreur Unauthorized 
-          //                          }
-          //                     })
-          //                     .catch(error => res.status(500).json({ error })) //Erreur Internal Server Error
-          //                     res.status(201).json({ message: 'Utilisateur créé !' })  
-          //                })
-          //                .catch(error => req.status(500).json({ error })) //Erreur Internal Server Error (de mongo)
-     // User.find()
-     // .then(users => {
-     //      const userExist = []
-     //      users.forEach(user => {
-     //           console.log("Si user existe au signup", user)
-     //           bcrypt.compare(req.body.email, user.email)  
-     //           .then(valid => {
-     //                if(valid){
-     //                     userExist.push(user)
-     //                     console.log("Tableau userExist avec l'user valid", userExist)
-     //                     console.log("Tableau userExist avec l'user valid", userExist.length)
-     //                     if(userExist.length >= 1){
-     //                          return res.status(401).json({ error: 'Utilisateur existant !' }) //Erreur Unauthorized
-     //                     }
-     //                     return
-     //                }
-     //           })
-     //           .catch(error => res.status(500).json({ error })) //Erreur Internal Server Error
-     //      })
-     // })
-     // .catch(error => req.status(500).json({ error })) //Erreur Internal Server Error (de mongo)
-     
+               .catch(error => res.status(500).json({ error }))   
+               // else if (userExist.length === 0) {
+                    // console.log("valid si compare invalid", valid)
+                    // else { //Si comparaison invalid on créé un user
+                    
+                    // }
+               // }
+          }
+     })
+     .catch(error => req.status(500).json({ error }))
 }
 
 //On exporte notre middleware login
@@ -111,7 +119,7 @@ exports.login = (req, res, next) => { //Middleware login
      console.log("req login", req.body)
      User.find() //Sort tableau de tous les users
      .then(users => {
-          console.log("Nombre de users", users.length)
+          console.log("Nombre de users au login", users.length)
           if (users.length === 0) { //Si non
                return res.status(401).json({ error: 'Utilisateur non trouvé !' }) //Erreur Unauthorized
           }
